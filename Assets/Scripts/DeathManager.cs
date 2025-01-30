94c8ace1f4e2fc52441642ebd8d06ebb7d85dc72
Joan Mendo <joan.mendo.7e7@itb.cat>
2025-01-30 14:56:15 +0100

    Ahora va mejor
    Sardina

--- Assets/Scripts/DeathManager.cs
+++ Assets/Scripts/DeathManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class DeathManager : MonoBehaviour
 {
     public Transform respawnPosition;
     public float deathForce;
     public static DeathManager Instance;
+    private GameObject globalVolume;
+    private float vignetteIntensity = 0.2f;
 
 
     private void Awake()
@@ -19,6 +23,7 @@ public class DeathManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject); // Opcional: Mantener entre escenas
+        globalVolume = GameObject.Find("Global Volume");
     }
 
 
@@ -38,10 +43,27 @@ public class DeathManager : MonoBehaviour
         player.GetComponent<FirstPersonController>().enabled = false;
         player.GetComponent<Rigidbody>().freezeRotation = false;
         player.GetComponent<Rigidbody>().AddForce(transform.up * deathForce, ForceMode.Impulse);
+        Coroutine cr = StartCoroutine(vingetteIntensity());
         yield return new WaitForSeconds(5);
         player.transform.position = respawnPosition.position;
+        StopCoroutine(cr);
+        globalVolume.GetComponent<Volume>().profile.TryGet(out Vignette vignette);
+        vignette.intensity.value = vignetteIntensity;
         player.GetComponent<Rigidbody>().velocity = Vector3.zero;
         player.GetComponent<Rigidbody>().freezeRotation = true;
         player.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    public IEnumerator vingetteIntensity()
+    {
+        globalVolume.GetComponent<Volume>().profile.TryGet(out Vignette vignette);
+        float intensity = vignette.intensity.value;
+
+        while (intensity < 1)
+        {
+            intensity += 0.015f;
+            vignette.intensity.value = intensity;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
