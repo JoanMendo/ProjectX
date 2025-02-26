88f23dac15ce8828cf143676a04dfd94aed8e51d
Joan Mendo <joan.mendo.7e7@itb.cat>
2025-02-26 16:47:13 +0100

    En proceso de añadir el inverted hull en los objetos

---
+++ Assets/Scripts/HighlightObjects.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObjects : MonoBehaviour
+{
+    public Camera playerCamera;
+    public LayerMask layers;
+    public Material highlightMaterial;
+
+    private GameObject currentObject = null;
+    // Update is called once per frame
+    void Update()
+    {
+        HighlightObjectsWithMaterial();
+    }
+    void HighlightObjectsWithMaterial()
+    {
+
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 2.5f, layers))
+        {
+            Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                if (currentObject == null)
+                {
+                    currentObject = hit.collider.gameObject;
+
+                    Material[] material = hit.collider.GetComponent<MeshRenderer>().materials;
+                    Material[] newMaterials = new Material[material.Length + 1];
+                    for (int i = 0; i < material.Length; i++)
+                    {
+                        newMaterials[i] = material[i]; // Copia los materiales antiguos
+                    }
+                    newMaterials[newMaterials.Length - 1] = highlightMaterial; // Añade el nuevo material
+
+                    hit.collider.GetComponent<MeshRenderer>().materials = newMaterials;
+                }  
+            }
+            if (currentObject != null && (hit.collider.GetComponent<Rigidbody>() == null) || hit.collider.GetComponent<Rigidbody>() != currentObject.GetComponent<Rigidbody>())
+            {
+                Material[] material = currentObject.GetComponent<MeshRenderer>().materials;
+                Material[] newMaterials = new Material[material.Length - 1];
+                for (int i = 0; i < newMaterials.Length; i++)
+                {
+                    newMaterials[i] = material[i]; // Copia los materiales antiguos
+                }
+                currentObject.GetComponent<MeshRenderer>().materials = newMaterials; // Elimina el material de resaltado
+                currentObject = null;
+            }
+        }
+        
+    }
+}
